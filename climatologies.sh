d12ac42e18c79acd5d5aa85919f0c5a2d508fcfc
sharkinsspatial <sharkinsgis@gmail.com>
2023-01-17 16:02:17 -0600

    Add climatologies wrapper script.

---
+++ climatologies.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Make sure that /var/lasrc_aux is a mountpoint
+lasrc_directory="$LASRC_AUX_DIR"
+echo "Checking mount status"
+mount | grep -q "$lasrc_directory" || exit 1
+cd "$lasrc_directory" || exit 1
+mkdir -p viirs
+cd viirs || exit 1
+ls 
+df -h
+
+echo "running generate_monthly_climatology.py"
+generate_monthly_climatology.py
+
+if [ -n "$LAADS_BUCKET" ]; then
+  echo "Syncing data to s3 bucket s3://$LAADS_BUCKET/lasrc_aux/viirs/"
+  aws s3 sync . "s3://$LAADS_BUCKET/lasrc_aux/viirs/"
+fi
